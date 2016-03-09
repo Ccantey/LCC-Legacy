bad299caa61d260002fde08829aa1f333921ea9c
ccantey <cantey0701@gmail.com>
2016-03-09 12:46:21 -0600

    css stuff/remove bad variable in php

---
+++ php/getOverlayLayersAsGeoJSON-sample.php
@@ -0,0 +1,40 @@
+<?php
+ 
+//database login info
+# Connect to PostgreSQL database
+$conn = new PDO('pgsql:host=####;dbname=####','####','####'); 
+
+$sql = "SELECT nid,title,fiscal_year, fy_funding, recipient, administrator, 
+       source, status, public.ST_AsGeoJSON(public.ST_Transform(the_geom,4326),6) AS geojson 
+        FROM legacy_pts_mar2016";
+
+$rs = $conn->query($sql);
+if (!$rs) {
+    echo 'An SQL error occured.\n';
+    exit;
+}
+# Build GeoJSON feature collection array
+$geojson = array(
+   'type'      => 'FeatureCollection',
+   'features'  => array()
+);
+
+# Loop through rows to build feature arrays
+while ($row = $rs->fetch(PDO::FETCH_ASSOC)) {
+    $properties = $row;
+    # Remove geojson and geometry fields from properties
+    unset($properties['geojson']);
+    unset($properties['the_geom']);
+    $feature = array(
+         'type' => 'Feature',
+         'geometry' => json_decode($row['geojson'], true),
+         'properties' => $properties
+    );
+    # Add feature arrays to feature collection array
+    array_push($geojson['features'], $feature);
+}
+
+header('Content-type: application/json');
+echo json_encode($geojson, JSON_NUMERIC_CHECK  );
+$conn = NULL;
+?>
