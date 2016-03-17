6925f92f0b593e76a8da04192b42edd8b15433f9
ccantey <cantey0701@gmail.com>
2016-03-17 12:38:36 -0500

    adjust favicon

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-    <title>Legacy</title>
+    <title>Legacy Map</title>
   </head>
 
   <body>
